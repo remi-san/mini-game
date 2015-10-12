52cb4a50b57b370978766e69e9e5ec0f0acd7cc0
Rémi San <remi.san@gmail.com>
2015-10-12 22:35:42 +0200

    removed unused methods from repositories

--- src/Repository/MiniGameRepository.php
+++ src/Repository/MiniGameRepository.php
@@ -19,40 +19,6 @@ interface MiniGameRepository
     public function find($id);
 
     /**
-     * Finds all minigames in the repository.
-     *
-     * @return MiniGame[] The minigames.
-     */
-    public function findAll();
-
-    /**
-     * Finds minigames by a set of criteria.
-     *
-     * Optionally sorting and limiting details can be passed. An implementation may throw
-     * an UnexpectedValueException if certain values of the sorting or limiting details are
-     * not supported.
-     *
-     * @param array      $criteria
-     * @param array|null $orderBy
-     * @param int|null   $limit
-     * @param int|null   $offset
-     *
-     * @return MiniGame[] The minigames.
-     *
-     * @throws \UnexpectedValueException
-     */
-    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null);
-
-    /**
-     * Finds a single minigame by a set of criteria.
-     *
-     * @param array $criteria The criteria.
-     *
-     * @return MiniGame The minigame.
-     */
-    public function findOneBy(array $criteria);
-
-    /**
      * Saves a mini game
      *
      * @param  MiniGame $game
--- src/Repository/PlayerRepository.php
+++ src/Repository/PlayerRepository.php
@@ -16,40 +16,6 @@ interface PlayerRepository
     public function find($id);
 
     /**
-     * Finds all players in the repository.
-     *
-     * @return Player[] The players.
-     */
-    public function findAll();
-
-    /**
-     * Finds players by a set of criteria.
-     *
-     * Optionally sorting and limiting details can be passed. An implementation may throw
-     * an UnexpectedValueException if certain values of the sorting or limiting details are
-     * not supported.
-     *
-     * @param array      $criteria
-     * @param array|null $orderBy
-     * @param int|null   $limit
-     * @param int|null   $offset
-     *
-     * @return Player[] The players.
-     *
-     * @throws \UnexpectedValueException
-     */
-    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null);
-
-    /**
-     * Finds a single player by a set of criteria.
-     *
-     * @param array $criteria The criteria.
-     *
-     * @return Player The player.
-     */
-    public function findOneBy(array $criteria);
-
-    /**
      * Saves a player
      *
      * @param  Player $player
